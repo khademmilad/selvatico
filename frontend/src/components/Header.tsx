087985d6d31833263295e3147f1d1ecf17fd4d35
Jawid <41361157+infjay@users.noreply.github.com>
2023-07-02 21:15:10 +0200

    initial frontend infrastructure

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,17 @@
+"use client"
 import React from 'react'
+import Navbar from './Navbar'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <div>
+    <Navbar />
+    <div className='' id='header'> 
+      <h1 className="text-center">"Revolutionizing Customer Support: Empowering Users, Redefining Satisfaction with Innovative Solutions!"</h1>
+    </div>
+    </div>
   )
 }
 
