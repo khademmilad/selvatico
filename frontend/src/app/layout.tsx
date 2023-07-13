32c9ef4ce8b2e5075f427067edf0e8e4620e88cf
Jawid <41361157+infjay@users.noreply.github.com>
2023-07-13 19:04:18 +0200

    about and dashboard initial

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import { Montserrat } from 'next/font/google'
+import Header from "./components/Header";
+import "./globals.css";
+import { Montserrat } from "next/font/google";
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Selvatico',
-  description: 'Revolutionazing User Support',
-}
+  title: "Selvatico",
+  description: "Revolutionazing User Support",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" >
-      <body className={montserrat.className}>{children}</body>
+    <html lang="en">
+      <body className={montserrat.className}>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
-
