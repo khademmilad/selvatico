d51a291e25e27c43dec1e1057e582c82cfd1ac56
Jawid <41361157+infjay@users.noreply.github.com>
2023-07-14 22:16:28 +0200

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,17 @@
-import RootLayout from "./layout";
+
 import Features from "./features/page";
 
 import Banner from "./components/Banner";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+    
       <Banner />
-      
+
       <Features />
 
-     
-       </>
+    </>
   );
 }
