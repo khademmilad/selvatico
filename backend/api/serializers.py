4122554544f7ec72697742ab28d84bffdbd6f9a0
khademmilad <khademmilad@gmail.com>
2023-07-14 15:09:08 +0200

    created login and register api endpoint

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,34 @@
-from rest_framework import serializers
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer, LoginSerializer
 
-class HelloWorldSerializer(serializers.Serializer):
-    message = serializers.CharField(default="Hello World")
+
+class RegistrationAPIView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {
+                'user': serializer.data,
+                'message': 'User registered successfully.',
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+
+class LoginAPIView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(
+            {
+                'user': serializer.validated_data,
+                'message': 'User logged in successfully.',
+            },
+            status=status.HTTP_200_OK
+        )
