be8b0745ccef85baca621cedef6cb4115eac4de2
Jawid <41361157+infjay@users.noreply.github.com>
2023-07-15 16:31:41 +0200

    update

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,15 +1,19 @@
-"use client"
-import  React,{useEffect, useState} from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import login from "../../../public/login.png";
 import Link from "next/link";
+
 type Props = {};
 
 const Login = (props: Props) => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const handleSubmit = () => {
-console.log("submitted")
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "http://141.95.0.236:8000/api/login/"
+  
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    
+  };
   return (
     <section className="max-sm:p-16 p-16 max-w-7xl mx-auto h-screen">
       <div className="h-full">
@@ -76,18 +80,29 @@ console.log("submitted")
                 </p>
               </div>
 
-              
-        <div className="w-full p-3">
-          <label className="block">
-            <input className="p-4 w-full text-gray-700 tracking-tight bg-white placeholder-gray-700 outline-none border border-gray-600 rounded-lg focus:border-indigo-500 transition duration-200" id="signUpInput1-2" type="email" placeholder="Email Address"/>
-          </label>
-        </div>
-        <div className="w-full p-3">
-          <label className="block">
-            <input className="p-4 w-full text-gray-700 tracking-tight bg-white placeholder-gray-700 outline-none border border-gray-600 rounded-lg focus:border-indigo-500 transition duration-200" id="signUpInput1-3" type="password" placeholder="Password"/>
-          </label>
-        </div>
-      
+              <div className="w-full p-3">
+                <label className="block">
+                  <input
+                    className="p-4 w-full text-gray-700 tracking-tight bg-white placeholder-gray-700 outline-none border border-gray-600 rounded-lg focus:border-indigo-500 transition duration-200"
+                    id="email"
+                    type="email"
+                    placeholder="Email Address"
+                    onChange={(e)=> setEmail(e.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="w-full p-3">
+                <label className="block">
+                  <input
+                    className="p-4 w-full text-gray-700 tracking-tight bg-white placeholder-gray-700 outline-none border border-gray-600 rounded-lg focus:border-indigo-500 transition duration-200"
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e)=> setPassword(e.target.value)}
+                  />
+                </label>
+              </div>
+
               <div className="mb-6 flex items-center justify-between max-sm:text-xs">
                 <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem] ">
                   <input
@@ -104,12 +119,14 @@ console.log("submitted")
                   </label>
                 </div>
 
-                <Link className="hover:text-purple-300" href="passwordreset ">Forgot password?</Link>
+                <Link className="hover:text-purple-300" href="passwordreset ">
+                  Forgot password?
+                </Link>
               </div>
 
               <div className="text-center lg:text-left">
                 <button
-                  type="button"
+                  type="submit"
                   className=" bg-indigo-500 hover:bg-indigo-600 focus:ring-indigo-300 inline-block rounded bg-primary px-7 pb-2.5 pt-3 max-sm:mb-2 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                   data-te-ripple-init
                   data-te-ripple-color="light"
@@ -119,8 +136,11 @@ console.log("submitted")
 
                 <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
                   Don't have an account? <span> </span>
-                  <Link href="register" className="hover:text-purple-300 text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700">
-                   Register
+                  <Link
+                    href="register"
+                    className="hover:text-purple-300 text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
+                  >
+                    Register
                   </Link>
                 </p>
               </div>
